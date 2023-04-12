ebc7e2c3d22036c5d92b803eb5cb7d4a8bfe1f78
LucasCavalei <59570195+LucasCavalei@users.noreply.github.com>
2023-04-12 20:20:45 -0300

    Add url Railway to mysql connect it

--- server/utils/database.js
+++ server/utils/database.js
@@ -3,11 +3,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-});
+const connection = mysql.createConnection(process.env.URLRailway);
+// const connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+//   database: process.env.DB_NAME,
+// });
 export default connection;
