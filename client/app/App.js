9c979a593e382461769d4150c97e60b27e2dd634
LucasCavalei <59570195+LucasCavalei@users.noreply.github.com>
2023-05-17 22:20:38 -0300

    delete unsed app folder, which was kept after mergin some merge conflics

--- client/app/App.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './App.scss';
-import Router from './Router';
-
-const App = () => <Router />;
-
-export default App;
--- client/app/components/sidebar/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './index.scss';
-
-export const Sidebar = () => {
-  return <div className="sidebar-container">Sidebar</div>;
-};
