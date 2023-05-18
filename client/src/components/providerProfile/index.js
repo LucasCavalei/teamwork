a31730f289f4e7cf6f0a3c4628b3c26cf0ff4482
LucasCavalei <59570195+LucasCavalei@users.noreply.github.com>
2023-05-17 22:11:06 -0300

    add logout button while mous over profile

--- client/src/components/providerProfile/index.js
+++ client/src/components/providerProfile/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.scss';
 import { useSelector } from 'react-redux';
 
 export const ProviderProfile = () => {
   const { authUser: provider } = useSelector((state) => state.user);
+  const [isMouseOver, setIsMouseOver] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsMouseOver(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsMouseOver(false);
+  };
 
   if (!provider) {
     return null;
@@ -13,13 +22,20 @@ export const ProviderProfile = () => {
     console.log(provider._json);
   }
   return (
-    <div className="providerProfile">
-      <img
-        className="provider-image"
-        src={provider._json.picture}
-        alt={provider._json.name}
-      />
-      <p style={{ margin: 0 }}>{provider._json.given_name}</p>
+    <div>
+      <div
+        className="providerProfile"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <img
+          className="provider-image"
+          src={provider._json.picture}
+          alt={provider._json.name}
+        />
+        <p style={{ margin: 0 }}>{provider._json.given_name}</p>
+      </div>
+      {isMouseOver ? <button>Logout</button> : ''}
     </div>
   );
 };
