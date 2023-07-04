284eff8f491527d310deb0f0adedce08bd289988
LucasCavalei <59570195+LucasCavalei@users.noreply.github.com>
2023-07-04 16:56:41 -0300

    add readMe with all description including Sequence Diagram and preview gif

--- client/src/components/providerProfile/index.js
+++ client/src/components/providerProfile/index.js
@@ -2,26 +2,28 @@ import React, { useState } from 'react';
 import './index.scss';
 import { useSelector, useDispatch } from 'react-redux';
 import { logout } from '../../redux/authSlice';
+import spinningCircle from '../assets/spinningCircle.json';
+import Lottie from 'react-lottie';
 
 export const ProviderProfile = () => {
   const dispatch = useDispatch();
   const { provider, authUser } = useSelector((state) => state.user);
   const [isMouseOver, setIsMouseOver] = useState(false);
-  const buttonStyles = {
-    backgroundColor: '#fca311',
-    color: 'white',
-    padding: '10px 20px',
-    border: 'none',
-    borderRadius: '4px',
-    cursor: 'pointer',
-    float: 'left',
-    position: 'relative',
-    left: '10px',
-    top: '30px',
+  const [showLottie, setShowLottie] = useState(false);
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: spinningCircle,
   };
+
   const handleLogout = () => {
-    dispatch(logout());
+    setShowLottie(true); //
+    setTimeout(() => {
+      dispatch(logout());
+    }, 2200);
   };
+
   const handleMouseEnter = () => {
     setIsMouseOver(true);
   };
@@ -35,6 +37,13 @@ export const ProviderProfile = () => {
   if (!provider && !authUser) {
     return null;
   }
+  const LottieComponent = () => {
+    return (
+      <div className="lottie-container">
+        <Lottie options={defaultOptions} height={145} width={145} />
+      </div>
+    );
+  };
 
   return (
     <div>
@@ -53,7 +62,7 @@ export const ProviderProfile = () => {
             <p style={{ margin: 0 }}>{provider.firstName}</p>
           </div>
           {isMouseOver && (
-            <button style={buttonStyles} onClick={handleLogout}>
+            <button className="buttonStyle" onClick={handleLogout}>
               Logout
             </button>
           )}
@@ -69,13 +78,14 @@ export const ProviderProfile = () => {
               <p style={{ margin: 0 }}>{authUser.user.name}</p>
             </div>
             {isMouseOver && (
-              <button style={buttonStyles} onClick={handleLogout}>
+              <button className="buttonStyle" onClick={handleLogout}>
                 Logout
               </button>
             )}
           </div>
         )
       )}
+      {showLottie ? <LottieComponent /> : null}
     </div>
   );
 };
--- client/src/components/sidebar/index.js
+++ client/src/components/sidebar/index.js
@@ -1,29 +1,11 @@
 import React from 'react';
 import './index.scss';
 import { ProviderProfile } from '../providerProfile';
-import { useDispatch } from 'react-redux';
-import { logout } from '../../redux/authSlice';
 
 export const Sidebar = () => {
   return (
     <div className="sidebar-container">
       <ProviderProfile />
-      {/* <LogoutButton />  */}
     </div>
   );
 };
-
-// const LogoutButton = () => {
-//   const dispatch = useDispatch();
-
-//   const handleLogout = () => {
-//     // Dispatch the logout action
-//     dispatch(logout());
-//     // dispatch(setIsAuthenticated(false))
-//     // dispatch(setAuthUser(""));
-
-//     // TODO: Call your logout API endpoint to invalidate the user's session
-//   };
-
-//   return <button onClick={handleLogout()}>Logout</button>;
-// };
